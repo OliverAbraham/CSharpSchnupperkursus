6f85c750d3ffd33626a135a6d6e675d50be26623
Oliver Abraham <mail@oliver-abraham.de>
2022-10-07 11:30:01 +0200

    Initial check-in

---
+++ QuizAppConsole/Program.cs
@@ -0,0 +1,59 @@
+﻿Console.WriteLine(@"   ____        _       ");
+Console.WriteLine(@"  / __ \      (_)      ");
+Console.WriteLine(@" | |  | |_   _ _ ____  ");
+Console.WriteLine(@" | |  | | | | | |_  /  ");
+Console.WriteLine(@" | |__| | |_| | |/ /   ");
+Console.WriteLine(@"  \___\_\\__,_|_/___|  ");
+Console.WriteLine(@"                       ");
+// gemacht mit https://onlineasciitools.com/convert-text-to-ascii-art
+
+Console.WriteLine();
+Console.WriteLine("Welche Naturschutzorganisation führt einen Panda in ihrem Logo?");
+Console.WriteLine("A Greenpeace");
+Console.WriteLine("B Robin Wood");
+Console.WriteLine("C WWF");
+Console.WriteLine("D NABU");
+var auswahl = Console.ReadLine();
+if (auswahl.ToUpper() == "C")
+	Console.WriteLine("richtig!");
+else
+	Console.WriteLine("leider falsch!");
+	
+Console.WriteLine();
+Console.WriteLine("Welche Tiere übernehmen in George Orwells 'Farm der Tiere' das Kommando auf dem Hof?");
+Console.WriteLine("A Die Pferde");
+Console.WriteLine("B Die Schweine");
+Console.WriteLine("C Die Hunde");
+Console.WriteLine("D Die Schafe");
+auswahl = Console.ReadLine();
+if (auswahl.ToUpper() == "B")
+	Console.WriteLine("richtig!");
+else
+	Console.WriteLine("leider falsch!");
+	
+Console.WriteLine();
+Console.WriteLine("Wie heißt die größte heimische Wespenart?");
+Console.WriteLine("A Hornisse");
+Console.WriteLine("B Deutsche Wespe");
+Console.WriteLine("C Biene");
+Console.WriteLine("D Hummel");
+auswahl = Console.ReadLine();
+if (auswahl.ToUpper() == "A")
+	Console.WriteLine("richtig!");
+else
+	Console.WriteLine("leider falsch!");
+	
+Console.WriteLine();
+Console.WriteLine("Welche Frucht schoss Wilhelm Tell mit der Armbrust vom Kopf seines Sohnes?");
+Console.WriteLine("A Banane");
+Console.WriteLine("B Birne");
+Console.WriteLine("C Apfel");
+Console.WriteLine("D Kirsche");
+auswahl = Console.ReadLine();
+if (auswahl.ToUpper() == "C")
+	Console.WriteLine("richtig!");
+else
+	Console.WriteLine("leider falsch!");
+
+Console.WriteLine();
+Console.WriteLine("Ende!");
---
+++ QuizAppWPF/QuizAppWPF/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace QuizAppWPF
+{
+    public partial class MainWindow : Window
+    {
+        private List<Frage> _quizfragen = new List<Frage>()
+        {
+            new Frage("", "", "", "", "", ""),
+
+            new Frage("Welche Naturschutzorganisation führt einen Panda in ihrem Logo?", 
+                "A Greenpeace", 
+                "B Robin Wood", 
+                "C WWF", 
+                "D NABU",
+                "C"),
+
+            new Frage("Welche Tiere übernehmen in George Orwells 'Farm der Tiere' das Kommando auf dem Hof?", 
+                "A Die Pferde",
+                "B Die Schweine",
+                "C Die Hunde",
+                "D Die Schafe",
+                "B"),
+
+            new Frage("Wie heißt die größte heimische Wespenart?", 
+                "A Hornisse",
+                "B Deutsche Wespe",
+                "C Biene",
+                "D Hummel",
+                "A"),
+
+            new Frage("Welche Frucht schoss Wilhelm Tell mit der Armbrust vom Kopf seines Sohnes?", 
+                "A Banane",
+                "B Birne",
+                "C Apfel",
+                "D Kirsche",
+                "C")
+        };
+
+        private int _aktuelleFrage = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            ZeigeNächsteFrage();
+        }
+
+        private void ZeigeNächsteFrage()
+        {
+            if (Frage is null)
+                return;
+
+            _aktuelleFrage++;
+            if (_aktuelleFrage >= _quizfragen.Count)
+            {
+                Frage.Text = "Ende";
+                AntwortA.Visibility = Visibility.Collapsed;
+                AntwortB.Visibility = Visibility.Collapsed;
+                AntwortC.Visibility = Visibility.Collapsed;
+                AntwortD.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Frage.Text       = _quizfragen[_aktuelleFrage].Fragetext;
+                AntwortA.Content = _quizfragen[_aktuelleFrage].Antworten[1];
+                AntwortB.Content = _quizfragen[_aktuelleFrage].Antworten[2];
+                AntwortC.Content = _quizfragen[_aktuelleFrage].Antworten[3];
+                AntwortD.Content = _quizfragen[_aktuelleFrage].Antworten[4];
+            }
+        }
+
+        private void AntwortA_Click(object sender, RoutedEventArgs e) { Reaktion("A"); }
+        private void AntwortB_Click(object sender, RoutedEventArgs e) { Reaktion("B"); }
+        private void AntwortC_Click(object sender, RoutedEventArgs e) { Reaktion("C"); }
+        private void AntwortD_Click(object sender, RoutedEventArgs e) { Reaktion("D"); }
+
+        private void Reaktion(string tipp)
+        {
+            if (_quizfragen[_aktuelleFrage].RichtigeAntwort == tipp)
+                MessageBox.Show("Richtig !");
+            else
+                MessageBox.Show("Leider falsch !");
+            ZeigeNächsteFrage();
+        }
+    }
+
+    internal class Frage
+    {
+        public string   Fragetext       { get; set; }
+        public string[] Antworten       { get; set; }
+        public string   RichtigeAntwort { get; set; }
+
+        public Frage(string fragetext, string antwortA, string antwortB, string antwortC, string antwortD, string richtigeAntwort)
+        {
+            Fragetext       = fragetext;
+            Antworten       = new string[5];
+            Antworten[1]    = antwortA;
+            Antworten[2]    = antwortB;
+            Antworten[3]    = antwortC;
+            Antworten[4]    = antwortD;
+            RichtigeAntwort = richtigeAntwort;
+        }
+    }
+}
